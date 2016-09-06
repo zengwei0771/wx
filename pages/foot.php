96b882c85a56d2794dc8e4e3f5686a526b3605cc
root <zengwei0771@126.com>
2016-09-06 15:17:23 +0800

    cnzz tongji

---
+++ pages/foot.php
@@ -0,0 +1,11 @@
+<div class="txt">
+    <ul class="info">
+        <li class="sitename"><a href="http://<?php echo $site_domain;?>/"><?php echo $site_name;?>&copy;<?php echo date('Y');?></a></li>
+        <li><a href="http://www.miitbeian.gov.cn/" target="_blank"><?php echo $beian;?></a></li>
+        <li><script src="http://s95.cnzz.com/z_stat.php?id=1260323568&web_id=1260323568" language="JavaScript"></script></li>
+    </ul>
+    <div class="state">
+        <?php echo $state;?>
+        <p>联系人：<a href="mailto:<?php echo $mail;?>"><?php echo $mail;?></a></p>
+    </div>
+</div>
--- pages/video.php
+++ pages/video.php
@@ -109,17 +109,7 @@
         </div>
     </div>
     <div class="foot">
-        <div class="txt">
-            <ul class="info">
-                <li class="sitename"><a href="http://<?php echo $site_domain;?>/"><?php echo $site_name;?>&copy;<?php echo date('Y');?></a></li>
-                <li><a href="http://www.miitbeian.gov.cn/" target="_blank"><?php echo $beian;?></a></li>
-                <li><script src="" language="JavaScript"></script></li>
-            </ul>
-            <div class="state">
-                <?php echo $state;?>
-                <p>联系人：<a href="mailto:<?php echo $mail;?>"><?php echo $mail;?></a></p>
-            </div>
-        </div>
+        <?php include 'foot.php';?>
     </div>
 </body>
 </html>
