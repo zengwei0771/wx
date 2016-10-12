6ec6125566ef245915b0e3fd750fdd81f2bff71e
zengwei0771 <zengwei0771@126.com>
2016-10-12 10:51:37 +0800

    u

--- pages/video.php
+++ pages/video.php
@@ -75,6 +75,14 @@
                 ?>
                 </ul>
             </div>
+            <div id="ad1" style="margin-bottom:20px;">
+                <script type="text/javascript">
+                    var sogou_ad_id=698655;
+                    var sogou_ad_height=200;
+                    var sogou_ad_width=360;
+                </script>
+                <script type='text/javascript' src='http://images.sohu.com/cs/jsfile/js/c.js'></script>
+            </div>
             <div id="hot_read" class="box">
                 <h3>推荐热点</h3>
                 <ul>
