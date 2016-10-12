7056c3dbc806676cccb8d53e526acfaf1d78b5ca
zengwei0771 <zengwei0771@126.com>
2016-10-12 10:45:38 +0800

    u

--- pages/leftnavi.php
+++ pages/leftnavi.php
@@ -6,6 +6,14 @@
     <li><a href="/videos/" title="微信文章视频合集">视&nbsp;&nbsp;&nbsp;频</a></li>
 </ul>
 <div>
+    <script type="text/javascript">
+    var sogou_ad_id=698680;
+    var sogou_ad_height=160;
+    var sogou_ad_width=160;
+    </script>
+    <script type='text/javascript' src='http://images.sohu.com/cs/jsfile/js/c.js'></script>
+</div>
+<div>
     <input class="search" placeholder="搜一搜"></input>
 </div>
 <div class="qrcode"><img src="/static/qrcode.png" width="160" /><a href="http://<?php echo $m_site_domain;?>/" title="<?php echo $site_name?>移动站">扫一扫 手机版</a></div>
